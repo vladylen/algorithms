54ce125bd7315ab6b953ec24e1ea54de0f654c1d
Vlad Shashkov <vladylen.shashkov@gmail.com>
2013-10-16 01:07:30 +0300

    implementation

--- src/Brute.java
+++ src/Brute.java
@@ -1,31 +1,78 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Comparator;
 
 public class Brute {
     public static void main(String[] args) {
-        /*Point q = new Point(0, 0);
-        Point w = new Point(6, 4);
-        Point e = new Point(3, 3);
-        Point r = new Point(10, 2);
-        */
-        Point q = new Point(0, 0);
-        Point w = new Point(1, 1);
-        Point e = new Point(3, 3);
-        Point r = new Point(10, 10);
+        args = new String[1];
+        args[0] = "input6.txt";
+        draw(args[0]);
 
-        q.draw();
-        w.draw();
-        e.draw();
-        r.draw();
+        if (args.length > 0) {
+            String argFileName = args[0];
 
-        q.drawTo(w);
-        w.drawTo(e);
-        e.drawTo(r);
-        r.drawTo(q);
+            try {
+                In in = new In(argFileName);
+                int N = in.readInt();
+                Point[] Points = new Point[N];
 
-        if (q.slopeTo(w) == q.slopeTo(e) && q.slopeTo(w) == q.slopeTo(r)) {
-            StdOut.println("Yes!");
+                for (int i = 0; i < N; i++) {
+                    Points[i] = new Point(in.readInt(), in.readInt());
+                    StdOut.println(Points[i].toString());
+                }
+
+                calculation(Points);
+            } catch (Exception ex) {
+                StdOut.println(ex.toString());
+            }
         } else {
-            StdOut.println("Nop!");
+            StdOut.println("main() - No arguments.");
         }
+        /*
+        if (q.slopeTo(w) == q.slopeTo(e) && q.slopeTo(w) == q.slopeTo(r)) {
+        }
+        */
+    }
+
+    private static void calculation(Point[] Points) {
+        for (int i = 0; i < Points.length; i++) {
+            for (int k = i + 1; k < Points.length; k++) {
+                for (int l = k + 1; l < Points.length; l++) {
+                    for (int m = l + 1; m < Points.length; m++) {
+                        double slope = Points[i].slopeTo(Points[k]);
+                        if ((Points[i].slopeTo(Points[l]) == slope) && (Points[i].slopeTo(Points[m]) == slope)) {
+                            StdOut.println("quadro");
+                            StdOut.println(Points[i].toString());
+                            StdOut.println(Points[k].toString());
+                            StdOut.println(Points[l].toString());
+                            StdOut.println(Points[m].toString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static void draw(String filename) {
+        // rescale coordinates and turn on animation mode
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        StdDraw.show(0);
+
+        // read in the input
+        In in = new In(filename);
+        int N = in.readInt();
+        Point prevPoint = new Point(0, 0);
+
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            Point p = new Point(x, y);
+            p.draw();
+            prevPoint.drawTo(p);
+        }
+
+        // display to screen all at once
+        StdDraw.show(0);
     }
 }
