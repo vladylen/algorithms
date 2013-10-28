cd53bb3d707da8e6b4c5d0ec276fcada5cd71aa5
Vlad Shashkov <vladylen.shashkov@gmail.com>
2013-10-28 02:32:28 +0200

    node

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,35 @@
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 public class Board {
     private int N;
+    private int emptyPosition;
+    private Node[][] blocks;
+
+    // helper linked list class
+    private static class Node<Integer> {
+        private Integer value = null;
+        private Integer position = null;
+        private Boolean empty = false;
+    }
 
     public Board(int[][] blocks)           // construct a board from an N-by-N array of blocks
     {
         N = blocks.length;
+        this.blocks = new Node[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // else {
+                this.blocks[i][j] = new Node();//(blocks[i][j]);
+                this.blocks[i][j].value = blocks[i][j];
+                this.blocks[i][j].position = position(i, j);
+                //}
+                if (blocks[i][j] == 0) {
+                    this.emptyPosition = position(i, j);
+                    this.blocks[i][j].empty = true;
+                }
+            }
+        }
     }
 
     // (where blocks[i][j] = block in row i, column j)
@@ -17,7 +40,19 @@ public class Board {
 
     public int hamming()                   // number of blocks out of place
     {
-        return 1;
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!this.blocks[i][j].empty) {
+                    if (this.blocks[i][j].position != this.blocks[i][j].value) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
     }
 
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
@@ -41,11 +76,6 @@ public class Board {
         return true;
     }
 
-    public String toString()               // string representation of the board (in the output format specified below)
-    {
-        return "";
-    }
-
     public Iterable<Board> neighbors()       // sequence of boards in a shortest solution; null if no solution
     {
         return new DequeIterator();
@@ -54,6 +84,26 @@ public class Board {
     private class DequeIterator implements Iterable<Board> {
         public Iterator<Board> iterator() {
             return null;
+        }
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(N).append("\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", blocks[i][j].value, blocks[i][j].position));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    private int position(int i, int j) {
+        if (i < N && j < N && i >= 0 && j >= 0) {
+            return i * N + j + 1;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
         }
     }
 }
--- src/Solver.java
+++ src/Solver.java
@@ -27,7 +27,36 @@ public class Solver {
         }
     }
 
-    public static void main(String[] args)  // solve a slider puzzle (given below)
-    {
+    public static void main(String[] args) {
+        /**/
+        args = new String[1];
+        args[0] = "puzzle04.txt";
+        /**/
+
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        StdOut.println(initial);
+        StdOut.println(initial.hamming());
+
+        /*
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+        */
+
     }
 }
