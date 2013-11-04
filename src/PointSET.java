f642723da3241291b5ec08c8ffc5707fd656f104
Vlad Shashkov <vladylen.shashkov@gmail.com>
2013-11-04 16:02:40 +0200

    5. Begin add classes with correct API

---
+++ src/PointSET.java
@@ -0,0 +1,40 @@
+public class PointSET {
+    private Queue<Point2D> solution;
+
+    public PointSET()                               // construct an empty set of points
+    {
+    }
+
+    public boolean isEmpty()                        // is the set empty?
+    {
+        return true;
+    }
+
+    public int size()                               // number of points in the set
+    {
+        return 1;
+    }
+
+    public void insert(Point2D p)                   // add the point p to the set (if it is not already in the set)
+    {
+    }
+
+    public boolean contains(Point2D p)              // does the set contain the point p?
+    {
+        return true;
+    }
+
+    public void draw()                              // draw all of the points to standard draw
+    {
+    }
+
+    public Iterable<Point2D> range(RectHV rect)     // all points in the set that are inside the rectangle
+    {
+        return solution;
+    }
+
+    public Point2D nearest(Point2D p)               // a nearest neighbor in the set to p; null if set is empty
+    {
+        return p;
+    }
+}
