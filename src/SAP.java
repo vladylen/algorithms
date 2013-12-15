b25eba551a5273102c5a4826a947e058e6646d45
Vlad Shashkov <vladylen.shashkov@gmail.com>
2013-12-16 00:47:50 +0200

    2.1 some fixes in sap

--- src/SAP.java
+++ src/SAP.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Field;
-
 public class SAP {
     private Digraph diGraph;
 
@@ -19,40 +17,78 @@ public class SAP {
     }
 
     private int graphInfo(int v, int w, String type) {
-        Iterable<Integer> shortestPath = null;
-        int ancestor = -1;
-        int size = -1;
+        Iterable<Integer> directShortestPath;
+        int directAncestor = -1;
+        int directSize = -1;
+        int rootAncestor = -1;
+        int rootSize = -1;
+        Iterable<Integer> rootShortestPath1;
+        int rootAncestor1 = -1;
+        int rootSize1 = -1;
+        Iterable<Integer> rootShortestPath2;
+        int rootAncestor2 = -1;
+        int rootSize2 = -1;
         int root = getRoot(v);
+        int ancestor;
+        int size;
 
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(diGraph, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(diGraph, w);
 
         // Direct path from v to w or from w to v
         if (bfsV.hasPathTo(w)) {
-            shortestPath = bfsV.pathTo(w);
-            size = getSize(shortestPath);
-            ancestor = w;
+            directShortestPath = bfsV.pathTo(w);
+            directSize = getSize(directShortestPath);
+            directAncestor = w;
         } else if (bfsW.hasPathTo(v)) {
-            shortestPath = bfsW.pathTo(v);
-            size = getSize(shortestPath);
-            ancestor = v;
+            directShortestPath = bfsW.pathTo(v);
+            directSize = getSize(directShortestPath);
+            directAncestor = v;
         }
 
-        if (shortestPath == null) {
-            if (bothHasPathTo(bfsV, bfsW, root)) {
-                Iterable<Integer> pathV2Top = bfsV.pathTo(root);
-                int additionalSize = 0;
+        // Path throw root vertex
+        if (bothHasPathTo(bfsV, bfsW, root)) {
+            Iterable<Integer> pathV2Top = bfsV.pathTo(root);
+            int additionalSize = 0;
 
-                for (Integer vertex : pathV2Top) {
-                    if (bfsW.hasPathTo(vertex)) {
-                        shortestPath = bfsW.pathTo(vertex);
-                        size = getSize(shortestPath) + additionalSize;
-                        ancestor = vertex;
-                        break;
-                    }
-                    additionalSize++;
+            for (Integer vertex : pathV2Top) {
+                if (bfsW.hasPathTo(vertex)) {
+                    rootShortestPath1 = bfsW.pathTo(vertex);
+                    rootSize1 = getSize(rootShortestPath1) + additionalSize;
+                    rootAncestor1 = vertex;
+                    break;
                 }
+                additionalSize++;
             }
+
+            Iterable<Integer> pathW2Top = bfsW.pathTo(root);
+            additionalSize = 0;
+
+            for (Integer vertex : pathW2Top) {
+                if (bfsV.hasPathTo(vertex)) {
+                    rootShortestPath2 = bfsV.pathTo(vertex);
+                    rootSize2 = getSize(rootShortestPath2) + additionalSize;
+                    rootAncestor2 = vertex;
+                    break;
+                }
+                additionalSize++;
+            }
+
+            if (rootSize1 > -1 && rootSize1 < rootSize2) {
+                rootSize = rootSize1;
+                rootAncestor = rootAncestor1;
+            } else {
+                rootSize = rootSize2;
+                rootAncestor = rootAncestor2;
+            }
+        }
+
+        if (directSize > -1 && directSize < rootSize) {
+            size = directSize;
+            ancestor = directAncestor;
+        } else {
+            size = rootSize;
+            ancestor = rootAncestor;
         }
 
         if (type.equals("length")) {
@@ -62,9 +98,20 @@ public class SAP {
         }
     }
 
+
     private int getRoot(int v) {
+        SET<Integer> visited = new SET<Integer>();
+        return getRoot(v, visited);
+    }
+
+    private int getRoot(int v, SET<Integer> visited) {
         for (Integer vertex : diGraph.adj(v)) {
-            return getRoot(vertex);
+            if (!visited.contains(vertex)) {
+                visited.add(vertex);
+                return getRoot(vertex, visited);
+            } else {
+                return v;
+            }
         }
 
         return v;
@@ -75,19 +122,12 @@ public class SAP {
     }
 
     private int getSize(Iterable<Integer> shortestPath) {
-        try {
-            Field field = shortestPath.getClass().getDeclaredField("N");
-            field.setAccessible(true);
-            try {
-                return field.getInt(shortestPath) - 1;
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+        int size = -1;
+        for (Integer ignored : shortestPath) {
+            size++;
         }
 
-        return -1;
+        return size;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
@@ -102,7 +142,7 @@ public class SAP {
 
     // for unit testing of this class (such as the one below)
     public static void main(String[] args) {
-        boolean test = true;
+        boolean test = false;
 
         if (test) {
             args = new String[1];
@@ -129,7 +169,7 @@ public class SAP {
         while (!StdIn.isEmpty()) {
             int v = StdIn.readInt();
             int w = StdIn.readInt();
-            int length   = sap.length(v, w);
+            int length = sap.length(v, w);
             int ancestor = sap.ancestor(v, w);
             StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
         }
