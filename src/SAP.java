4a8cb2b763483ff74c63dc788699543b959523b2
Vlad Shashkov <vladylen.shashkov@gmail.com>
2013-12-16 19:55:23 +0200

    2.1 implement WordNet.class

--- src/SAP.java
+++ src/SAP.java
@@ -7,183 +7,97 @@ public class SAP {
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w) {
+    public int length(int v, int w) throws IndexOutOfBoundsException {
         return getGraphInfo(v, w, "length");
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
+    public int ancestor(int v, int w) throws IndexOutOfBoundsException {
         return getGraphInfo(v, w, "ancestor");
     }
 
-    private int getGraphInfo(int v, int w, String type) {
-        SET<Integer> root1 = getRoot(v);
-        int root = 0;
-        int ancestor;
-        int size;
-
-        GraphInfo rootGraphInfo;
-        GraphInfo directGraphInfo;
+    private int getGraphInfo(int v, int w, String type) throws IndexOutOfBoundsException {
+        if (v < 0 || v >= diGraph.V() || w < 0 || w >= diGraph.V()) {
+            throw new IndexOutOfBoundsException();
+        }
 
         BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(diGraph, v);
         BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(diGraph, w);
 
-        directGraphInfo = getDirectPath(v, w, bfsV, bfsW);
-        rootGraphInfo = getPathTroughRoot(root, bfsV, bfsW);
-
-        if (directGraphInfo.size > -1 && ((rootGraphInfo.size > -1 && directGraphInfo.size < rootGraphInfo.size) || (directGraphInfo.size > -1 && rootGraphInfo.size < 0))) {
-            size = directGraphInfo.size;
-            ancestor = directGraphInfo.ancestor;
-        } else {
-            size = rootGraphInfo.size;
-            ancestor = rootGraphInfo.ancestor;
-        }
+        GraphInfo graphInfo = processGraph(bfsV, bfsW);
 
         if (type.equals("length")) {
-            return size;
+            return graphInfo.length;
         } else {
-            return ancestor;
+            return graphInfo.ancestor;
         }
     }
 
-    private SET<Integer> getRoot(int v) {
-        SET<Integer> roots = new SET<Integer>();
-
-        for (Integer vertex : diGraph.adj(v)) {
-            SET<Integer> visited = new SET<Integer>();
-            visited.add(vertex);
-            int rootVertex = getRoot(vertex, visited);
-
-            if (!roots.contains(rootVertex)) {
-                roots.add(rootVertex);
+    private int getGraphInfo(Iterable<Integer> v, Iterable<Integer> w, String type) throws IndexOutOfBoundsException {
+        for (Integer value : v) {
+            if (value < 0 || value >= diGraph.V()) {
+                throw new IndexOutOfBoundsException();
             }
-
         }
-
-        return roots;
-    }
-
-    private int getRoot(int v, SET<Integer> visited) {
-        for (Integer vertex : diGraph.adj(v)) {
-            if (!visited.contains(vertex)) {
-                visited.add(vertex);
-                v = getRoot(vertex, visited);
-            } else {
-                return v;
+        for (Integer value : w) {
+            if (value < 0 || value >= diGraph.V()) {
+                throw new IndexOutOfBoundsException();
             }
         }
 
-        return v;
-    }
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(diGraph, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(diGraph, w);
 
-    private GraphInfo getDirectPath(int v, int w, BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW) {
-        GraphInfo directGraphInfo = new GraphInfo();
+        GraphInfo graphInfo = processGraph(bfsV, bfsW);
 
-        if (bfsV.hasPathTo(w)) {
-            directGraphInfo.shortestPath = bfsV.pathTo(w);
-            directGraphInfo.size = getSize(directGraphInfo.shortestPath);
-            directGraphInfo.ancestor = w;
-        } else if (bfsW.hasPathTo(v)) {
-            directGraphInfo.shortestPath = bfsW.pathTo(v);
-            directGraphInfo.size = getSize(directGraphInfo.shortestPath);
-            directGraphInfo.ancestor = v;
+        if (type.equals("length")) {
+            return graphInfo.length;
+        } else {
+            return graphInfo.ancestor;
         }
-
-        return directGraphInfo;
     }
 
-    private GraphInfo getPathTroughRoot(int root, BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW) {
-        if (bothHasPathTo(bfsV, bfsW, root)) {
-            GraphInfo rootGraphInfoV = getPathToRoot(root, bfsW, bfsV);
-            GraphInfo rootGraphInfoW = getPathToRoot(root, bfsV, bfsW);
+    private GraphInfo processGraph(BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW) {
+        GraphInfo graphInfo = new GraphInfo();
 
-            if (rootGraphInfoV.size > -1 && rootGraphInfoV.size < rootGraphInfoW.size) {
-                return rootGraphInfoV;
-            } else {
-                return rootGraphInfoW;
-            }
-        }
+        for (int i = 0; i < diGraph.V(); i++) {
+            if (bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
+                int tmpLength = bfsV.distTo(i) + bfsW.distTo(i);
 
-        return new GraphInfo();
-    }
-
-    private GraphInfo getPathToRoot(int root, BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW) {
-        int additionalSize;
-        Iterable<Integer> pathToRoot = bfsW.pathTo(root);
-        additionalSize = 0;
-
-        GraphInfo rootGraphInfo = new GraphInfo();
-        for (Integer vertex : pathToRoot) {
-            if (bfsV.hasPathTo(vertex)) {
-                GraphInfo tmpGraphInfo = new GraphInfo();
-                tmpGraphInfo.shortestPath = bfsV.pathTo(vertex);
-                tmpGraphInfo.size = getSize(tmpGraphInfo.shortestPath) + additionalSize;
-                tmpGraphInfo.ancestor = vertex;
-
-                if (rootGraphInfo.size == -1) {
-                    rootGraphInfo = tmpGraphInfo;
-                } else if (tmpGraphInfo.size < rootGraphInfo.size) {
-                    rootGraphInfo = tmpGraphInfo;
+                if (graphInfo.length == -1 || (tmpLength < graphInfo.length)) {
+                    graphInfo.length = tmpLength;
+                    graphInfo.ancestor = i;
                 }
             }
-            additionalSize++;
         }
 
-        return rootGraphInfo;
-    }
-
-    private class GraphInfo {
-        public Iterable<Integer> shortestPath = null;
-        public int size = -1;
-        public int ancestor = -1;
-    }
-
-    private boolean bothHasPathTo(BreadthFirstDirectedPaths bfsV, BreadthFirstDirectedPaths bfsW, int root) {
-        return bfsV.hasPathTo(root) && bfsW.hasPathTo(root);
-    }
-
-    private int getSize(Iterable<Integer> shortestPath) {
-        int size = -1;
-        for (Integer ignored : shortestPath) {
-            size++;
-        }
-
-        return size;
+        return graphInfo;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 1;
+        return getGraphInfo(v, w, "length");
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 1;
+        return getGraphInfo(v, w, "ancestor");
+    }
+
+    private class GraphInfo {
+        public int length = -1;
+        public int ancestor = -1;
     }
 
     // for unit testing of this class (such as the one below)
     public static void main(String[] args) {
-        boolean test = true;
+        boolean test = false;
 
         if (test) {
             args = new String[1];
             args[0] = "C:\\Users\\vlsh\\Dropbox\\Algorithms\\wordnet\\digraph5.txt";
         }
 
-        /*
-        StdOut.println("ancestor(3, 2) = " + sap.ancestor(3, 2));
-        StdOut.println("length(3, 2) = " + sap.length(3, 2));
-        StdOut.println("ancestor(3, 0) = " + sap.ancestor(3, 0));
-        StdOut.println("length(3, 0) = " + sap.length(3, 0));
-        StdOut.println("ancestor(3, 11) = " + sap.ancestor(3, 11));
-        StdOut.println("length(3, 11) = " + sap.length(3, 11));
-        StdOut.println("ancestor(5, 11) = " + sap.ancestor(5, 11));
-        StdOut.println("length(5, 11) = " + sap.length(5, 11));
-        StdOut.println("ancestor(5, 5) = " + sap.ancestor(5, 5));
-        StdOut.println("length(5, 5) = " + sap.length(5, 5));
-        StdOut.println("ancestor(5, 6) = " + sap.ancestor(5, 6));
-        StdOut.println("length(5, 6) = " + sap.length(5, 6));
-        */
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
         SAP sap = new SAP(G);
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,6 +1,81 @@
 public class WordNet {
+    private SAP sap;
+    private Queue<Node> hypernyms = new Queue<Node>();
+    private SeparateChainingHashST<String, Sunset> sunsetsHashST = new SeparateChainingHashST<String, Sunset>(10);
+
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
+        readSunsets(synsets);
+        readHypernyms(hypernyms);
+
+        Digraph digraph = new Digraph(sunsetsHashST.size());
+        for (Node node : this.hypernyms) {
+            digraph.addEdge(node.v, node.w);
+        }
+
+        sap = new SAP(digraph);
+        StdOut.println(sap.length(0, 10));
+        StdOut.println(sap.length(1, 10));
+    }
+
+    private void readHypernyms(String hypernymsFile) {
+        In inH = new In(hypernymsFile);
+        while (!inH.isEmpty()) {
+            String line = inH.readLine();
+            String[] fields = line.split(",");
+
+            int v = -1;
+            for (int i = 0; i < fields.length; i++) {
+                int w = -1;
+                if (i == 0) {
+                    v = Integer.parseInt(fields[i]);
+                } else {
+                    w = Integer.parseInt(fields[i]);
+                }
+
+                if (w > -1) {
+                    Node node = new Node(v, w);
+                    hypernyms.enqueue(node);
+                }
+            }
+        }
+    }
+
+    private void readSunsets(String synsets) {
+        In inS = new In(synsets);
+        while (!inS.isEmpty()) {
+            String line = inS.readLine();
+            String[] fields = line.split(",");
+
+            int id = Integer.parseInt(fields[0]);
+            String sunset = fields[1];
+            String description = fields[2];
+
+            Sunset item = new Sunset(id, sunset, description);
+            sunsetsHashST.put(id + sunset, item);
+        }
+    }
+
+    private class Node {
+        public int v;
+        public int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    private class Sunset {
+        public int id;
+        public String sunset;
+        public String description;
+
+        public Sunset(int id, String sunset, String description) {
+            this.id = id;
+            this.sunset = sunset;
+            this.description = description;
+        }
     }
 
     // the set of nouns (no duplicates), returned as an Iterable
@@ -26,5 +101,14 @@ public class WordNet {
 
     // for unit testing of this class
     public static void main(String[] args) {
+        boolean test = true;
+
+        if (test) {
+            args = new String[2];
+            args[0] = "C:\\Users\\vlsh\\Dropbox\\Algorithms\\wordnet\\synsets15.txt";
+            args[1] = "C:\\Users\\vlsh\\Dropbox\\Algorithms\\wordnet\\hypernymsPath15.txt";
+        }
+
+        WordNet wordnet = new WordNet(args[0], args[1]);
     }
 }
